b70b5331b8c8314896c26e4e545a4dbed167aab5
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-09-30 12:44:21 +0800

    problem9

--- problem9.py
+++ problem9.py
@@ -2,16 +2,16 @@
 # exactly one pythagorean triplet where a + b + c = 1000
 # find a,b,c
 
-# can find all permutation of a,b,c which add up to give 1000
+# can find all combination of a,b,c which add up to give 1000
 # check if any of them are pythagorean triplets
 
 
 # a + b + c = 1000
 # natural numbers
-# least value of a,b = 1 implies third one is 998
+# least value of a,b,c = 1 implies most is 998 (since none can be 0)
 # iterate from 1 to 998 and 1 to 998 for a,b
 # c = 1000 - (a + b)
-# if answer gives a positive integer, then one possible combination of a,b,c is found
+# if c is also between 1 and 998, then one possible combination of a,b,c is found
 # can try storing in sets to avoid repitition 
 # then check each for pythagorean triplet
 
