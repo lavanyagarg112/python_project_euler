4f73b19c17ebf05ecd5629169be0d77b625cd0c4
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-10-01 08:50:50 +0800

    added more search criteria

--- problem9.py
+++ problem9.py
@@ -28,13 +28,18 @@ s = set()
 
 start = time.time()
 
-for a in range(1, 998):
+for a in range(1, 999, 2): # make this odd
 
-    for b in range(1, 998):
+    for b in range(2, 999, 2): # make this even
 
         c = 1000 - (a + b)
-        if c in range(1, 998):
-            s.add((a,b,c))
+        if c in range(1, 999, 2): # make this odd
+            if a % 3 == 0 or b % 3 == 0:
+                if b % 4 == 0: # a is odd so cannot be divisible by 4 anyway
+                    if a % 5 == 0 or b % 5 == 0 or c % 5 == 0:
+                        s.add((a,b,c))
+
+print(s)
 
 for i in s:
 
