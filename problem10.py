b8bd1ebc413572d0f22e547ebb25efbe9d0a41cd
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-10-01 09:21:28 +0800

    modified to make more efficient

--- problem10.py
+++ problem10.py
@@ -7,11 +7,14 @@
 # current complexity of erathosthenes seive: O(n^2)
 # required: O(nlog(log(n)))
 
-end = 10
+end = 200000
 output = range(2,end)
 
-for i in range(2, end):
-    output = list(filter(lambda x: x == i or x % i != 0, output))
-
+j = 2
+while j < end:
+    if j in output:
+        start = output.index(j) + 1
+        output = list(output[0:start]) + list(filter(lambda x: x % j != 0, output[start:]))
+    j += 1
 
 print(sum(output))
